79115b3a275cdaf015967b21e3b68e4132c20e65
KubaMilion <jakub.grzegorczyk@gmail.com>
2018-12-19 16:19:44 +0100

    random house placer

---
+++ Assets/Basics/Scripts/clickFunctionHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class clickFunctionHandler : MonoBehaviour
+{
+    List<Transform> prefabs = new List<Transform>();
+    public Transform House1;
+    public Transform House2;
+    public Transform House3;
+
+    Vector3 position;
+
+    System.Random rnd = new System.Random();
+
+    public clickFunctionHandler()
+    {
+    }
+    public void Tester(string input)
+    {
+        prefabs.Add(House1);
+        prefabs.Add(House2);
+        prefabs.Add(House3);
+        float x = rnd.Next(-20, 20) * 0.32f;
+        float y = rnd.Next(-15, 15) * 0.32f;
+        position = new Vector3(x, y, 1f);
+        Instantiate(prefabs[2], position, Quaternion.identity);
+        Debug.Log(input + " x = " + x + ", y = " + y);
+    }
+}
