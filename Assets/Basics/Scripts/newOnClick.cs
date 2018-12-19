11ec5c9e05224c21b2a316ec9b1f860b2d52d7c7
KubaMilion <jakub.grzegorczyk@gmail.com>
2018-12-19 12:05:02 +0100

    trash

--- Assets/Basics/Scripts/newOnClick.cs
+++ Assets/Basics/Scripts/newOnClick.cs
@@ -1,18 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class newOnClick : MonoBehaviour
 {
+    public Button CustomButton;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        Button button = CustomButton.GetComponent<Button>();
+        button.onClick.AddListener(CustomButton_onClick);
     }
 
     // Update is called once per frame
-    void Update()
+    void CustomButton_onClick()
     {
-        
+        Debug.Log("test");
     }
 }
